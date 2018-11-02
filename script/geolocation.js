756c6422ced22630d80725f9243f32fd3ded51a3
Mateus Ramos <Mateus.Ramos@fanatics.co.uk>
2018-11-02 12:29:34 +0000

    change geolocation

--- script/geolocation.js
+++ script/geolocation.js
@@ -5,55 +5,70 @@ const fs = require('fs');
 
 let json = [];
 
-function getGeoLocation(address) {   
+function getGeoLocation(addresses) {
     var options = {
-     provider: 'here',
-    
-     appId: 'xXk9XTGXpGB4Nrv7b1Zk',
-     appCode: 'ccQxGFU4LIjaKBj8NfZMmg',
-     formatter: 'gpx'
+        provider: 'here',
+
+        appId: 'xXk9XTGXpGB4Nrv7b1Zk',
+        appCode: 'ccQxGFU4LIjaKBj8NfZMmg',
+        formatter: 'gpx'
     };
-    
+
     var geocoder = nodeGeocoder(options);
 
-    geocoder.geocode(address)
-     .then(function(res) {
-        parseString(res, function (err, result) {
-            let geolocation = result.gpx.wpt[0].$;
-            json.push({
-                'lat': geolocation.lat,
-                'lng': geolocation.lon
-            });
-           });
-     })
-     .catch(function(err) {
-       console.log(err);
-     });
+    // for (let address of addresses) {
+
+    //     geocoder.geocode(address)
+    //     .then(function(res) {
+    //         parseString(res, function (err, result) {
+    //             let geolocation = result.gpx.wpt[0].$;
+    //             json.push({
+    //                 'lat': geolocation.lat,
+    //                 'lng': geolocation.lon
+    //             });
+    //         });
+    //     })
+    //     .catch(function(err) {
+    //     console.log(err);
+    //     });
+    // }
+    let promises = [];
+    for (let address of addresses) {
+        promises.push(geocoder.geocode(address));
+    }
+
+    Promise.all(promises)
+        .then(function (res) {
+            for (let r of res) {
+                parseString(res, function (err, result) {
+                    let geolocation = result.gpx.wpt[0].$;
+                    json.push({
+                        'lat': geolocation.lat,
+                        'lng': geolocation.lon
+                    });
+                });
+            }
+            console.log(json)
+        });
 };
 
 function convertCsv() {
     var stream = fs.createReadStream("data.csv");
     let lines = [];
-    
+
     csv
-     .fromStream(stream, {headers : false})
-     .on("data", function(data){
+        .fromStream(stream, { headers: false })
+        .on("data", function (data) {
             lines.push(data[0]);
-     })
-     .on("end", function(){
-         console.log("done reading csv file");
-
-         for (let line in lines) {
-            getGeoLocation(line);
-         }
-
-         console.log(json);
-     });
+        })
+        .on("end", function () {
+            console.log("done reading csv file");
+            getGeoLocation(lines);
+        });
 }
 
-convertCsv()
+convertCsv();
 //module.exports = getlaLon
 
 
 
-    
