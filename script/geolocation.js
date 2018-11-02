07d9eb99b4c7eebd6df6fc93b5fbe8b685975022
Mateus Ramos <Mateus.Ramos@fanatics.co.uk>
2018-11-02 13:20:00 +0000

    generate json file with locations

--- script/geolocation.js
+++ script/geolocation.js
@@ -5,24 +5,24 @@ const fs = require('fs');
 
 let json = [];
 
-function getGeoLocation(addresses) {
+function getGeoLocation(addresses) {   
     var options = {
         provider: 'here',
-
+    
         appId: 'xXk9XTGXpGB4Nrv7b1Zk',
         appCode: 'ccQxGFU4LIjaKBj8NfZMmg',
         formatter: 'gpx'
     };
-
+    
     var geocoder = nodeGeocoder(options);
     let promises = [];
-    for (let address of addresses) {
+    for(let address of addresses) {
         promises.push(geocoder.geocode(address));
     }
 
     Promise.all(promises)
-        .then(function (res) {
-            for (let r of res) {
+        .then(function(res) {
+            for(let r of res) {
                 parseString(r, function (err, result) {
                     let geolocation = result.gpx.wpt[0].$;
                     json.push({
@@ -31,25 +31,29 @@ function getGeoLocation(addresses) {
                     });
                 });
             }
-            console.log(json);
+            fs.writeFile("orders.json", json, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
         });
 };
 
 function convertCsv() {
     var stream = fs.createReadStream("data.csv");
     let lines = [];
-
+    
     csv
-        .fromStream(stream, { headers: false })
-        .on("data", function (data) {
+        .fromStream(stream, {headers : false})
+        .on("data", function(data){
             lines.push(data[0]);
         })
-        .on("end", function () {
+        .on("end", function(){
             getGeoLocation(lines);
         });
 }
 
-let json = convertCsv();
+convertCsv();
 //module.exports = getlaLon
 
 
